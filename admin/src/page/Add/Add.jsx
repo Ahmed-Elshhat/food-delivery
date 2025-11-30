8a80644ae890790bbbbfb0574fda0c1683ac4366
Ahmed Mohamed <a7medelshhat@gmail.com>
2025-11-30 13:21:55 +0200

    create user files for backend and create login and register api

--- admin/src/page/Add/Add.jsx
+++ admin/src/page/Add/Add.jsx
@@ -4,7 +4,7 @@ import { assets } from "../../assets/admin_assets/assets";
 import axios from "axios";
 import { toast } from "react-toastify";
 
-function Add() {
+function Add({url}) {
   const [image, setImage] = useState(false);
   const [data, setData] = useState({
     name: "",
@@ -12,8 +12,6 @@ function Add() {
     price: "",
     category: "Salad",
   });
-
-  const url = "http://localhost:5000";
 
   const onChangeHandler = (e) => {
     const name = e.target.name;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config"
 
 
 // app config
@@ -17,7 +19,9 @@ connectDB();
 
 // api endpoints
 app.use("/api/food", foodRouter)
+app.use("/api/user", userRouter)
 app.use("/images", express.static("uploads"))
+
 
 app.get("/", (req, res) => {
   res.send("API Working")
