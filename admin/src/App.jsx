e34b6c756c4482368c876cbafdf14bce8cbbde81
Ahmed Mohamed <a7medelshhat@gmail.com>
2025-11-30 11:51:48 +0200

    create server files and create food crud api and create admin app files and create add food page for admin app

---
+++ admin/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Add from "./page/Add/Add";
+import List from "./page/List/List";
+import Orders from "./page/Orders/Orders";
+import { ToastContainer } from "react-toastify";
+
+function App() {
+  return (
+    <div>
+      <ToastContainer />
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
